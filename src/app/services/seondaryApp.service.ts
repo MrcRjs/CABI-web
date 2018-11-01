49207462a22da7cdb75d57fc9230b20bb8c27ce9
LuisGerardo95 <luisjerry1995@hotmail.com>
2018-10-31 23:51:03 -0600

    Manual Tecnico

--- src/app/services/seondaryApp.service.ts
+++
@@ -1,4 +0,0 @@
-import * as firebase from 'firebase/app';
-import { environment } from '../../environments/environment';
-
-export const SecondaryApp = firebase.initializeApp (environment.firebase, 'Secondary');
