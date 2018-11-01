083cb074bc2391004f4bde9c474f5a81bde85a44
LuisGerardo95 <luisjerry1995@hotmail.com>
2018-11-01 01:45:37 -0600

    Anexo de Manual de Pruebas

--- src/app/models/binnacle.model.ts
+++
@@ -1,9 +0,0 @@
-export class Binnacle {
-    id?: string;
-    idBici?: string;
-    uid?: string;
-    fechaEntrada?: string;
-    fechaSalida?: string;
-    lugarEntrada?: string;
-    lugarSalida?: string;
-}
